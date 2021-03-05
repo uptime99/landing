7c9cf49fa4e6732106acb99dc69dbd7a98501985
Hamid FzM <hamidfzm@gmail.com>
2021-03-06 00:38:26 +0330

    fix: useTheme hook import

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTheme } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
 
 import { useTranslation } from 'next-i18next';
 import NextHead from 'next/head';
